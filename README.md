a1a59affae18eccf00b9957d4337dd545f683b86
TanTan <tzlgege@gmail.com>
2024-12-22 03:13:14 +0000

    feat: try Redfin Scraper and Home Harvest

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # flip-alert
+This is a tool for flippers to monitor the housing market and be alerted for potential listing that is suitable for flipping.
+
---
+++ src/redfin-scrapper.py
@@ -0,0 +1,17 @@
+'''Install the redfin package using "python3 -m pip install redfin" in your terminal'''
+from redfin import Redfin
+client = Redfin()
+
+print(type(client))
+
+address = '4544 Radnor St, Detroit Michigan'
+
+response = client.search(address)
+url = response['payload']['exactMatch']['url']
+initial_info = client.initial_info(url)
+
+property_id = initial_info['payload']['propertyId']
+mls_data = client.below_the_fold(property_id)
+
+listing_id = initial_info['payload']['listingId']
+avm_details = client.avm_details(property_id, listing_id)
